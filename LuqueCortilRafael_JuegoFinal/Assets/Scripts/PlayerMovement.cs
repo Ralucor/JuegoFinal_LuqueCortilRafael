40d247d10de783f5797e293e2e98cdfe7b2d82e7
Ralucor <rluqcor0106@g.educaand.es>
2025-02-12 13:39:22 +0100

    SpritePocho

--- LuqueCortilRafael_JuegoFinal/Assets/Scripts/PlayerMovement.cs
+++ LuqueCortilRafael_JuegoFinal/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] int speed = 5;
+    public int jump = 5;
     Rigidbody2D rb;
     void Start()
     {
@@ -15,5 +16,21 @@ public class PlayerMovement : MonoBehaviour
     {
         float entradaX = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(entradaX * speed, rb.linearVelocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space)&&touchGround()==true){
+            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+        }
+    }
+    private bool touchGround(){
+        RaycastHit2D touches = Physics2D.Raycast(
+            transform.position, Vector2.down,0.2f);
+
+        if(touches.collider == null)
+        {
+            return false;
+        } else{
+            return true;
+        }
+        
     }
 }
