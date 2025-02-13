aa232b0c234c966a33fd7eb058dfcd868bcf3955
Ralucor <rluqcor0106@g.educaand.es>
2025-02-13 14:33:12 +0100

    ProjectileWorks!

--- LuqueCortilRafael_JuegoFinal/Assets/Scripts/ProjectileControl.cs
+++ LuqueCortilRafael_JuegoFinal/Assets/Scripts/ProjectileControl.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using UnityEngine.WSA;
 
 public class ProjectileControl : MonoBehaviour
 {
     public float power= 5f;
     Rigidbody2D rb;
-
+    GameObject weapon;
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
+       weapon=GameObject.FindGameObjectWithTag("weapon");
+       
+       rb.AddForce(weapon.GetComponent<ShootingControl>().Launch * power, ForceMode2D.Impulse);
     }
     private void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.tag=="Limit"){
--- LuqueCortilRafael_JuegoFinal/Assets/Scripts/ShootingControl.cs
+++ LuqueCortilRafael_JuegoFinal/Assets/Scripts/ShootingControl.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class ShootingControl : MonoBehaviour
 {
     public Vector2 DragStart;
-    
+    public Vector2 DragEnd;
+    public Vector2 Launch;
+    [SerializeField] GameObject bullet;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +15,19 @@ public class ShootingControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)){
+            DragStart =Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if(Input.GetMouseButton(0)){
+
+        }
+        if(Input.GetMouseButtonUp(0)){
+            DragEnd = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Launch=(DragStart-DragEnd);
+            Instantiate(bullet,
+             new Vector3(transform.position.x, 
+             transform.position.y, 0),
+             Quaternion.identity);
+        }
     }
 }
