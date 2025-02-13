57dabce2687a0134677df710481190df01bf8579
Ralucor <rluqcor0106@g.educaand.es>
2025-02-13 14:41:33 +0100

    yes

--- LuqueCortilRafael_JuegoFinal/Assets/Scripts/ShootingControl.cs
+++ LuqueCortilRafael_JuegoFinal/Assets/Scripts/ShootingControl.cs
@@ -6,22 +6,23 @@ public class ShootingControl : MonoBehaviour
     public Vector2 DragEnd;
     public Vector2 Launch;
     [SerializeField] GameObject bullet;
+    LineRenderer lr;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        lr=GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)){
+        if (Input.GetMouseButtonDown(1)){
             DragStart =Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
-        if(Input.GetMouseButton(0)){
-
+        if(Input.GetMouseButton(1)){
+           
         }
-        if(Input.GetMouseButtonUp(0)){
+        if(Input.GetMouseButtonUp(1)){
             DragEnd = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Launch=(DragStart-DragEnd);
             Instantiate(bullet,
