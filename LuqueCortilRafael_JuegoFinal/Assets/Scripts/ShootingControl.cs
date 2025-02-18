d2c81b52d956e32a29ab453e9f8612a05faab0dd
Ralucor <rluqcor0106@g.educaand.es>
2025-02-18 13:22:00 +0100

    ...

--- LuqueCortilRafael_JuegoFinal/Assets/Scripts/ShootingControl.cs
+++ LuqueCortilRafael_JuegoFinal/Assets/Scripts/ShootingControl.cs
@@ -6,8 +6,6 @@ public class ShootingControl : MonoBehaviour
     public Vector2 DragEnd;
     public Vector2 Launch;
     [SerializeField] GameObject bullet;
-    [SerializeField] float trajectoryTimeStep = 0.05f;
-    [SerializeField] int trajectoryStepCount = 15;
     [SerializeField]LineRenderer lr;
     [SerializeField] GameObject weapon;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
